e209656de44c5877b3eec9ece98fa0b40cce7336
Martijn Aben <martijn.aben@gmail.com>
2025-03-22 18:41:23 +0100

    Add clock

--- debug.py
+++ debug.py
@@ -1,23 +1,31 @@
 from squares import TwinklySquares
 import asyncio
+import time
 
 async def main() -> None:
     # Connect to device
-    t = TwinklySquares(host="192.168.1.172",rows=2,cols=3)
+    t = TwinklySquares(host="192.168.1.172",layout=
+        [
+            [[1,0],[2,1],[3,3]],
+            [[0,0],[5,1],[4,1]],
+        ],
+        clock_x_offset=3,
+        clock_y_offset=5,
+        blink_speed=1000)
     await t.interview()
+    await t.set_brightness(5)
+    #await t.recalibrate()
+    #await t.calibrate_set_order([[1,2,3],[0,5,4]])
+    #await t.calibrate_rotate(1)
+    #await t.calibrate_rotate(2)
+    #await t.calibrate_rotate(2)
+    #await t.calibrate_rotate(2)
+    #await t.calibrate_rotate(4)
+    #await t.calibrate_rotate(5)
 
-    await t.recalibrate()
-
-    await t.calibrate_set_order([[1,2,3],[0,5,4]])
-
-    await t.calibrate_rotate(1)
-
-    await t.calibrate_rotate(2)
-    await t.calibrate_rotate(2)
-    await t.calibrate_rotate(2)
-
-    await t.calibrate_rotate(4)
-    await t.calibrate_rotate(5)
+    while True:
+        await t.render_full_frame()
+        time.sleep(60-time.localtime().tm_sec)
 
     await t.close()
 
--- squares.py
+++ squares.py
@@ -1,16 +1,19 @@
 from ttls.client import Twinkly
+import time
 BLACK = (0x00,0x00,0x00)
 RED = (0xFF,0x00,0x00)
 GREEN = (0x00,0xFF,0x00)
 
-from small import digits,font_width,font_height,x_offsets,y_offset,colon_x_offset,colon_y_offset
+from small import digits,font_width,font_height
 
 class TwinklySquares(Twinkly):
     
-    def __init__(self,host,rows=None,cols=None,layout=None):
+    def __init__(self,host,rows=None,cols=None,layout=None,clock_x_offset=1,clock_y_offset=1,blink_speed=1000):
         # Call the parent constructor
         super().__init__(host=host)
-        
+        self.clock_x_offset = clock_x_offset
+        self.clock_y_offset = clock_y_offset
+        self.blink_speed = blink_speed
         # If layout is provided, just set it
         if layout is not None: 
             self.layout = layout
@@ -61,6 +64,32 @@ class TwinklySquares(Twinkly):
         self.layout[index//self.cols][index%self.cols][1] = (self.layout[index//self.cols][index%self.cols][1]+1)%4
         await self.show_calibrate()
 
+    async def render_full_frame(self):
+        frame = [BLACK] * self.length
+        frame = self.render_clock(frame,self.clock_x_offset,self.clock_y_offset)
+        await self.send_movie(frame,2)
+
+    def render_clock(self,frame,x_offset,y_offset):
+        # Convert current time to four separate digits
+        now = time.localtime()
+        numbers = [now.tm_hour//10,now.tm_hour%10,now.tm_min//10,now.tm_min%10]
+        x_offsets = [0 + x_offset, font_width+1 + x_offset, 2*(font_width)+4 + x_offset, 3*(font_width)+5 + x_offset]
+        # For each of the four digits
+        for i in range(4):
+            # Get the number
+            num = numbers[i]
+            self.print_glyph(frame,num,x_offsets[i],y_offset)
+
+        # Duplicate the frame
+        frame = frame * 2
+        
+        # Add the colon to only the first frame
+        frame[self.xyToIndex(x_offsets[2]-2,y_offset+font_height//2-1)] = RED
+        frame[self.xyToIndex(x_offsets[2]-2,y_offset+font_height//2+1)] = RED
+
+        return frame
+        
+
     async def show_calibrate(self):
         frame = [BLACK] * self.length
         i = 0
@@ -69,12 +98,16 @@ class TwinklySquares(Twinkly):
                 self.print_glyph(frame,i,c*8,r*8)
                 frame[self.xyToIndex((c+1)*8-1,(r+1)*8-1)] = GREEN
                 i+=1
+        await self.send_movie(frame)
+
+    async def send_movie(self,frame,frames_number=1):
+        # Avoid flashing by first switching away from movie and then later back again
         await self.set_mode("rt")
         await self.set_movie_config(
             {
-                "frames_number": 1,
+                "frames_number": frames_number,
                 "loop_type": 0,
-                "frame_delay": 1,
+                "frame_delay": self.blink_speed,
                 "leds_number": self.length,
             }
         )
